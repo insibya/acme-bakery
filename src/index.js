272c8ba46f743ab355eb26a20e9b53a578f8e5b2
Nathan Roy <nathanmroy@yahoo.com>
2020-03-03 19:10:21 -0800

    created basic files (empty)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const { useState, useEffect } = React;
+
+const root = document.querySelector('#root');
+ReactDOM.render(<hr />, root);
